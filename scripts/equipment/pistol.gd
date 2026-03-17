86687f66d10e5f2626f3a9105f39c035e3ae1d29
DavidLee36 <david303lee@gmail.com>
2026-03-16 21:38:56 -0700

    environment adjustments and initial sounds

--- scripts/equipment/pistol.gd
+++ scripts/equipment/pistol.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @onready var anim_player = $Sketchfab_model/Root/Armature/AnimationPlayer
 @onready var ray = $RayCast3D
+@onready var audio_player = $AudioStreamPlayer3D
 
 @export var bullet_spread := 2
 
@@ -18,6 +19,10 @@ enum bulletType {
 func shoot(spread: bool) -> void:
 	if anim_player.is_playing(): return # shoot speed based on animation
 	anim_player.play("SlideBack")
+	audio_player.play()
+	_spawn_bullet(spread)
+
+func _spawn_bullet(spread: bool) -> void:
 	bullet = basic_bullet.instantiate()
 	bullet.shooter = owner
 	bullet.position = ray.global_position
